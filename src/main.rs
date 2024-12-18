ec4c8bac28a9c915e3ae1019d11f8a782b9fc5a7
LittleB0xes <nihenriot@gmail.com>
2024-12-18 18:58:50 +0100

    Add copy, paste and range copy paste

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use crossterm::{
     event::{read, Event, KeyCode},
     queue,
     style::{
-        Color::{Red, Reset, White, DarkYellow},
+        Color::{Red, Reset, White, DarkYellow, Green},
         Colors, Print, PrintStyledContent, Stylize,
     },
     terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
@@ -30,8 +30,11 @@ fn main() -> io::Result<()> {
     let mut refresh: bool = true;
     let mut exit: bool = false;
     let mut insert_mode: bool = false;
+    let mut selection_mode: bool = false;
     let mut cursor_index = 0;
     let mut nibble_index = 0;
+    let mut cursor_start = 0;
+    let mut clipboard: Vec<u8> = Vec::new();
     let mut stdout = io::stdout();
 
     let args: Vec<String> = env::args().collect();
@@ -45,7 +48,7 @@ fn main() -> io::Result<()> {
     let _ = enable_raw_mode();
     while !exit {
         if refresh {
-            render_screen(&mut stdout, &buffer, cursor_index, insert_mode)?;
+            render_screen(&mut stdout, &buffer, cursor_start, cursor_index, insert_mode, selection_mode)?;
             refresh = false;
         }
         let event = read()?;
@@ -56,10 +59,7 @@ fn main() -> io::Result<()> {
                         if k as u8 >= 48 && k as u8 <= 57 {
                             let value = k as u8 - 48; 
 
-                            let nibble_bits: u8 = value << 4 * (1 - nibble_index);
-                            let mask: u8 = 0x0F << 4 * nibble_index;
-                            buffer[cursor_index % 16 + 16 * (cursor_index / 16)] &= mask;
-                            buffer[cursor_index % 16 + 16 * (cursor_index / 16)] |= nibble_bits;
+                            write_nibble(&mut buffer, cursor_index % 16 + 16 * (cursor_index / 16), value, nibble_index);
                             nibble_index += 1;
                             if nibble_index > 1 {
                                 nibble_index = 0;
@@ -71,10 +71,7 @@ fn main() -> io::Result<()> {
                         }
                         else if k as u8 >= 97 && k as u8 <= 102 {
                             let value = k as u8 - 87; 
-                            let nibble_bits: u8 = value << 4 * (1 - nibble_index);
-                            let mask: u8 = 0x0F << 4 * nibble_index;
-                            buffer[cursor_index % 16 + 16 * (cursor_index / 16)] &= mask;
-                            buffer[cursor_index % 16 + 16 * (cursor_index / 16)] |= nibble_bits;
+                            write_nibble(&mut buffer, cursor_index % 16 + 16 * (cursor_index / 16), value, nibble_index);
                             nibble_index += 1;
                             if nibble_index > 1 {
                                 nibble_index = 0;
@@ -97,6 +94,7 @@ fn main() -> io::Result<()> {
                 KeyCode::Char('q') => exit = true,
                 KeyCode::Char('h') | KeyCode::Left => {
                     if cursor_index > 0 {
+                        nibble_index = 0;
                         cursor_index -= 1;
                         refresh = true;
                     }
@@ -104,17 +102,20 @@ fn main() -> io::Result<()> {
                 KeyCode::Char('j') | KeyCode::Down=> { 
                     if cursor_index < buffer.len() - 17 {
                         cursor_index += 16;
+                        nibble_index = 0;
                         refresh = true;
                     }
                 }
                 KeyCode::Char('k') | KeyCode::Up => {
                     if cursor_index >= 16 {
+                        nibble_index = 0;
                         cursor_index -= 16;
                         refresh = true;
                     }
                 }
                 KeyCode::Char('l') | KeyCode::Right => {
                     if cursor_index < buffer.len() - 1 {
+                        nibble_index = 0;
                         cursor_index += 1;
                         refresh = true;
                     }
@@ -122,23 +123,55 @@ fn main() -> io::Result<()> {
                 KeyCode::Char('0') => {
                     let line = cursor_index / 16;
                     cursor_index = line * 16;
+                    nibble_index = 0;
                     refresh = true;
                 }
                 KeyCode::Char('$') => {
                     let line = cursor_index / 16;
                     cursor_index = line * 16 + 15;
+                    nibble_index = 0;
                     refresh = true;
                 }
                 KeyCode::Char('g') => {
                     cursor_index = 0;
                     refresh = true;
+                    nibble_index = 0;
                 }
                 KeyCode::Char('G') => {
                     cursor_index = buffer.len() - 1;
+                    nibble_index = 0;
                     refresh = true;
                 }
                 KeyCode::Char('i') => {
                     insert_mode = true;
+                    selection_mode = false;
+                    refresh = true;
+                }
+                KeyCode::Char('y') => {
+                    clipboard.clear();
+                    if selection_mode {
+                        let clipboard_range = cursor_index  - cursor_start + 1;
+                        for n in 0..clipboard_range {
+                            clipboard.push(buffer[cursor_index - (clipboard_range - 1 - n)]);
+                        }
+                        selection_mode = false;
+                        refresh = true;
+                    
+                    } else {
+                        clipboard.push(buffer[cursor_index]);
+                    }
+                }
+                KeyCode::Char('p') => {
+                    for n in 0..clipboard.len() {
+                        if cursor_index + n < buffer.len() {
+                            buffer[cursor_index + n] = clipboard[n];
+                        }
+                    }
+                    refresh = true;
+                }
+                KeyCode::Char('v') => {
+                    cursor_start = cursor_index;
+                    selection_mode = true;
                     refresh = true;
                 }
                 KeyCode::Char('w') => {
@@ -147,13 +180,16 @@ fn main() -> io::Result<()> {
 
                 }
                 KeyCode::Esc => {
+                    nibble_index = 0;
                     insert_mode = false;
+                    selection_mode = false;
                     refresh = true;
                 }
                 _ => {}
             },
             _ => {}
         }
+
     }
 
     stdout
@@ -166,7 +202,14 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn render_screen(stdout: &mut Stdout, buffer: &Vec<u8>, cursor_index: usize, edit_mode: bool) -> io::Result<()> {
+fn write_nibble(buffer: &mut Vec<u8>, position: usize, value: u8, nibble_hl: u8) {
+    let nibble_bits: u8 = value << 4 * (1 - nibble_hl);
+    let mask: u8 = 0x0F << 4 * nibble_hl;
+    buffer[position] &= mask;
+    buffer[position] |= nibble_bits;
+}
+
+fn render_screen(stdout: &mut Stdout, buffer: &Vec<u8>, cursor_start: usize, cursor_index: usize, edit_mode: bool, selection_mode: bool) -> io::Result<()> {
     queue!(stdout, Clear(ClearType::UntilNewLine), cursor::Hide)?;
 
     let mut line: u16 = 0;
@@ -196,10 +239,13 @@ fn render_screen(stdout: &mut Stdout, buffer: &Vec<u8>, cursor_index: usize, edi
             )?;
         }
 
-        if i == cursor_index && !edit_mode {
+        if i == cursor_index && !edit_mode && !selection_mode {
             queue!(stdout, SetColors(Colors::new(White, Red)))?;
-        } else if i == cursor_index && edit_mode {
+        } else if i == cursor_index && edit_mode && !selection_mode{
             queue!(stdout, SetColors(Colors::new(White, DarkYellow)))?;
+
+        } else if selection_mode && i >= cursor_start && i <= cursor_index {
+            queue!(stdout, SetColors(Colors::new(White, Green)))?;
 
         } else {
             queue!(stdout, SetColors(Colors::new(Reset, Reset)))?;
